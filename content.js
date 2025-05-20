2fc61909dab021e0051af4f96a45caaf4cc85d73
Dimaldos <dvakopyan@yandex.ru>
2025-05-20 10:18:43 +0300

    init

---
+++ content.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    
+    document.querySelector("body > div.content-wrapper > main > div > article > div.flex-wrapper > div").className = '';
+    document.querySelector("body > div.content-wrapper > main > div > article > div.flex-wrapper").className = '';
+  
+});
