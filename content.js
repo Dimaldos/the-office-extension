bb8746f57d19eb35481476e4777134be068fbe20
Dimaldos <dvakopyan@yandex.ru>
2025-05-21 14:41:25 +0300

    a little bit wider

--- content.js
+++ content.js
@@ -1,6 +1,5 @@
 window.addEventListener('load', () => {
-    
     document.querySelector("body > div.content-wrapper > main > div > article > div.flex-wrapper > div").className = '';
     document.querySelector("body > div.content-wrapper > main > div > article > div.flex-wrapper").className = '';
-  
+    document.querySelector("body > div.content-wrapper > main").style.maxWidth = '1100px';
 });
