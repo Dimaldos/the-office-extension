e687864055f353e55dd769c44f98c15ec562fb98
Dimaldos <dvakopyan@yandex.ru>
2025-05-22 11:03:10 +0300

    minor fix

--- content.js
+++ content.js
@@ -1,5 +1,8 @@
 window.addEventListener('load', () => {
-    document.querySelector("body > div.content-wrapper > main > div > article > div.flex-wrapper > div").className = '';
-    document.querySelector("body > div.content-wrapper > main > div > article > div.flex-wrapper").className = '';
-    document.querySelector("body > div.content-wrapper > main").style.maxWidth = '1100px';
+    var el = document.querySelector("body > div.content-wrapper > main > div > article > div.flex-wrapper > div");
+    if (el) {
+        document.querySelector("body > div.content-wrapper > main > div > article > div.flex-wrapper > div").className = '';
+        document.querySelector("body > div.content-wrapper > main > div > article > div.flex-wrapper").className = '';
+        document.querySelector("body > div.content-wrapper > main").style.maxWidth = '1100px';
+    }
 });
